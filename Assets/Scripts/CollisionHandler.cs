3c016b6cca4cb96a9ab74b46a7dc82960ed6128f
Home PC <Home PC@DESKTOP-RNEKSF1>
2021-03-07 22:40:52 +0530

    Added finish and crash audio. Added code to load new levels.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -6,23 +6,76 @@ namespace Assets.Scripts
 {
     public class CollisionHandler : MonoBehaviour
     {
+        [SerializeField] float levelLoadDelayTime = 1f;
+        [SerializeField] AudioClip crashAudio;
+        [SerializeField] AudioClip successAudio;
+
+        AudioSource audioSource;
+        float initialVolume;
+
+        bool isTransitioning;
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+            initialVolume = audioSource.volume;
+        }
 
         private void OnCollisionEnter(Collision collision)
         {
-            switch(collision.gameObject.tag)
+            if (!isTransitioning)
             {
-                case ("Friendly"): Debug.Log("Friendly");
-                    break;
-                case ("Finish"): Debug.Log("Finish");
-                    break;
-                case ("Obstacle"): ReloadScene();
-                    break;
+                switch (collision.gameObject.tag)
+                {
+                    case ("Friendly"):
+                        Debug.Log("Friendly");
+                        break;
+                    case ("Finish"):
+                        SuccessHandler();
+                        break;
+                    case ("Obstacle"):
+                        CrashHandler();
+                        break;
+                }
             }
+        }
+
+        void SuccessHandler()
+        {
+            isTransitioning = true;
+            StopAllCoroutines();
+            GetComponent<Movement>().enabled = false;
+            audioSource.Stop();
+            audioSource.volume = initialVolume;
+            audioSource.PlayOneShot(successAudio);
+            Invoke("LoadNextLevel", levelLoadDelayTime);
+        }
+
+        void CrashHandler()
+        {
+            isTransitioning = true;
+            StopAllCoroutines();
+            GetComponent<Movement>().enabled = false;
+            audioSource.Stop();
+            audioSource.volume = initialVolume;
+            audioSource.PlayOneShot(crashAudio);
+            //GetComponent<Rigidbody>().gameObject.SetActive(false);
+            Invoke("ReloadScene", levelLoadDelayTime);
         }
 
         void ReloadScene()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        void LoadNextLevel()
+        {
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            {
+                nextSceneIndex = 0;
+            }
+            SceneManager.LoadScene(nextSceneIndex);
         }
     }
 }
